63a58fd9e6eaf2f79b1278e61c88ca122be3d118
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2026-03-06 18:54:59 -0400

    add scroll-top buttom

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 const boton = document.querySelector(".menu-btn");
 const nav = document.querySelector(".nav");
+const btnTop = document.getElementById("top-btn");
+
 
 boton.addEventListener("click", () =>{
     nav.classList.toggle("open");
 })
 
+
+document.querySelectorAll(".nav a").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        const id = link.getAttribute("href");
+        const section = document.querySelector(id);
+        section.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
+
+
+btnTop.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
